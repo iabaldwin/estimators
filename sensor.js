ee00520b90ec3117fefded4ef2cd98c4a153de53
Ian Baldwin <ian@iabaldwin.com>
2013-01-15 19:06:02 -0800

    Updates

--- sensor.js
+++ sensor.js
@@ -38,17 +38,20 @@ IAB.Sensors=  {
         var Red = new THREE.Color( 0xff0000 );
         var Green = new THREE.Color( 0x00ff00 );
 
+        var call_counter = 0;
         this.update = function( robot_location )
         {
-            //console.log( robot_location );
-            //var visible_landmarks =  this.tree.nearest( {x: 0, y: 0}, this.num_neighbors );
             var visible_landmarks =  this.tree.nearest( robot_location, this.num_neighbors, 100 );
-      
+    
+            if ((call_counter++ % 100) == 0)
+            {
+                this.addNode( robot_location );
+            }
+
             for ( var i = 0; i<visible_landmarks.length; i++ )
             {
                 var index = landmarks.points.indexOf( visible_landmarks[i][0] );
 
-                console.log( index );
                 landmarks.meshes[index].material.color = Green;
 
                 //this.line_geometries[i].geometry.vertices[1] = visible_landmarks[i];
@@ -56,6 +59,19 @@ IAB.Sensors=  {
                 //this.line_geometries[i].geometry.elementsNeedUpdate = true;
                 //console.log( this.line_geometries[i] );
             }
+        }
+
+        this.addNode = function(robot_location){
+            // Build vehicle representation
+            var geometry = new THREE.CircleGeometry( 5, 50 );
+            var material = new THREE.MeshBasicMaterial( { color: 0x0000ff, ambient: 0x00ff80, shading: THREE.FlatShading} );
+            var mesh    = new THREE.Mesh( geometry, material );
+            
+            scene.add( mesh );
+
+            mesh.position = new THREE.Vector3( robot_location.x, robot_location.y, robot_location.z );
+            mesh.rotation.x += THREE.Math.degToRad( 270 );
+
         }
     }
 }

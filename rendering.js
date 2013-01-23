5e16126003a4c4ab6a793461cbac422cf88e8c0a
Ian Baldwin <ian@iabaldwin.com>
2013-01-23 11:16:29 -0800

    Updates

--- rendering.js
+++ rendering.js
@@ -4,8 +4,6 @@ IAB.Tools = {
 
     Plotting: function() {
 
-       
-
         this.drawCovarianceEllipse = function( state, P, sigma_bound )
         {
             // Compute eigenvalue decomposition
@@ -17,12 +15,15 @@ IAB.Tools = {
             var y_space = numeric.linspace( 0, 2*Math.PI, 100 );
      
             x_space = x_space.map( function(x){ return Math.cos(x)} );
-            y_space = y_space.map( function(x){ return Math.cos(x)} );
-       
-            var f = Flotr.draw( 
+            y_space = y_space.map( function(x){ return Math.sin(x)} );
+
+            var zip =  _.zip( x_space, y_space );
+
+            var f = Flotr.draw(
                     $('container'), 
-                    [{data:x_space,
-                    data:y_space}]);
+                    [ zip ], 
+                    {points: {show: true}});
+                
         }
 
     }

0730d57124aad4a74cddeff16668bb54b2b5f88b
Ian Baldwin <ian@iabaldwin.com>
2013-01-27 10:31:14 -0800

    Updates

---
+++ dev/dev_mapping.js
@@ -0,0 +1,3 @@
+var map = new IAB.Mapping.GridMap().randomMap();
+
+
---
+++ mapping.js
@@ -0,0 +1,15 @@
+var IAB = window.IAB || {};
+
+IAB.Mapping = 
+{
+
+    GridMap: function()
+    {
+
+        this.randomMap = function(x,y)
+        {
+            return numeric.random([x,y]);
+        }
+
+    }
+};

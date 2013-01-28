2698f4ffd87e028f03a6e6d8c32e0758a2b467c7
Ian Baldwin <ian@iabaldwin.com>
2013-01-28 10:05:51 -0800

    Matrix square-root bug still exists

--- tools.js
+++ tools.js
@@ -11,17 +11,15 @@ IAB.Tools = {
             // Initialise circle
             var x_space = numeric.linspace( 0, 2*Math.PI, 100 );
             var y_space = numeric.linspace( 0, 2*Math.PI, 100 );
-     
+
+            // Scale
             x_space = x_space.map( function(x){ return 10*Math.cos(x)} );
             y_space = y_space.map( function(x){ return 10*Math.sin(x)} );
 
             var zip =  _.zip( x_space, y_space );
 
             // Compute eigenvalues
-            //var VD = numeric.eig(P) ;
             var VD = numeric.eig([ [P[0][0], P[0][1]], [P[1][0], P[1][1]] ]) ;
-
-            //console.log( numeric.prettyPrint( this.math.matrixSquareRoot( numeric.diag( VD.lambda.x.reverse()  ) ) ) );
 
             // Compute transformation
             transformation = numeric.dot( VD.E.x, this.math.matrixSquareRoot( numeric.diag( VD.lambda.x.reverse() ))) ;

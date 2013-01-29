2d5a7c62d55af569bb38b17328cefa4e42804f83
Ian Baldwin <ian@iabaldwin.com>
2013-01-29 15:14:53 -0800

    Updates

--- observations.js
+++ observations.js
@@ -3,52 +3,61 @@ var IAB = window.IAB || {};
 IAB.Observations = 
 {
 
-    RangingModel: function( landmarks )
+    RangingModel: function( landmarks, debug )
     {
-        var temp = new THREE.Vector3();
-
-        
-        var material = new THREE.LineBasicMaterial({
-            color: 0x0000ff,
-        });
-
-        var lines = [];
-        for ( var i=0; i<landmarks.length; i++ )
+        if (debug)
         {
-        
-            var geometry = new THREE.Geometry();
+            var material = new THREE.LineBasicMaterial({
+                color: 0x0000ff,
+            });
 
-            geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
-            geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
+            var lines = [];
+            for ( var i=0; i<landmarks.length; i++ )
+            {
+            
+                var geometry = new THREE.Geometry();
+
+                geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
+                geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
       
-            var line = new THREE.Line(geometry, material);
-          
-            scene.add(line);
+                var line = new THREE.Line(geometry, material);
+              
+                scene.add(line);
 
-            lines.push( line );
+                lines.push( line );
+            }
         }
 
 
         this.update = function( vehicle_position )
         {
-            // Subtract out the map
-            //var distances = landmarks.map( function(x){ return 1; } );
+            var tmp = new THREE.Vector3(), loc;
+
+            var observations = [];
 
             for( var i=0; i<landmarks.length; i++ )
             {
-                //var tmp = vehicle_position.sub( vehicle_position, landmarks[i] );
-                //console.log( landmarks[i] );
-                //break;
-           
-                var line = lines[i];
-           
-                line.geometry.vertices[0].copy( vehicle_position );
-                line.geometry.vertices[1].copy( landmarks[i].position );
+                // Subtract out the map
+                loc = tmp.sub( new THREE.Vector3( vehicle_position.x, 0, vehicle_position.y ), landmarks[i] );
 
-                line.geometry.verticesNeedUpdate = true;
+                var range = loc.length();
+
+                var angle = Math.atan2( loc.x, loc.y )  - vehicle_position.theta;
+
+                observations.push( {range:range, angle:angle} );
+
+                if (debug)
+                {
+                    var line = lines[i];
+
+                    line.geometry.vertices[0].copy( vehicle_position );
+                    line.geometry.vertices[1].copy( landmarks[i].position );
+
+                    line.geometry.verticesNeedUpdate = true;
+                }
             }
 
+            return observations;
         }
     }
-
 };
